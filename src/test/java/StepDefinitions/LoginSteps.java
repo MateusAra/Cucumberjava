1d1ddde36d48cdd2faafd9c6277d627274e6100c
MateusAra <mateus19araujo@hotmail.com>
2023-09-26 21:20:07 -0300

    adicionando funções de steps

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -1,5 +1,33 @@
 package StepDefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LoginSteps {
+	@Given("que o usuario esta na pagina de login")
+	public void que_o_usuario_esta_na_pagina_de_login() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("o usuario inserir seu nome de user e password")
+	public void o_usuario_inserir_seu_nome_de_user_e_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@And("clicar no botao de login")
+	public void clicar_no_botao_de_login() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("o usuario é redirecionado para a pagina home")
+	public void o_usuario_é_redirecionado_para_a_pagina_home() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 
 }
