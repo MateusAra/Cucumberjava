da1996985f0db6bf3529ce5fc76bc3e40acebc9a
MateusAra <mateus19araujo@hotmail.com>
2023-10-05 16:39:05 -0300

    implementando relatorios dos testes

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -5,7 +5,13 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features", glue="StepDefinitions", monochrome = true)
+@CucumberOptions(
+				features="src/test/resources/Features", 
+				glue="StepDefinitions", 
+				monochrome = true,
+				plugin = {"pretty","json:target/JSONReports/reports.json",
+									"html:target/HTMLReports/reports.html"}
+				)
 public class TestRunner {
 
 }
